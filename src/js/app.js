c94587c07538c1c64df5ac7ffc803e3125555096
Alexey Nazarov <nazaroff.ag@yandex.ru>
2022-01-31 08:03:32 +0300

    form elements added

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,29 @@
 import * as flsFunctions from "./modules/functions.js";
+import noUiSlider from "nouislider";
 
 flsFunctions.isWebp();
 
-console.log("Hello");
+console.log("Hello, js here");
 
 // подключение swiper
 import Swiper, { Navigation, Pagination } from "swiper";
 
 const swiper = new Swiper();
+
+// rangeSlider
+
+document.addEventListener("DOMContentLoaded", () => {
+  var slider = document.querySelector(".range-slider__slider");
+
+  if (slider) {
+    noUiSlider.create(slider, {
+      start: [20, 80],
+      connect: true,
+      // tooltips: [true, true],
+      range: {
+        min: 0,
+        max: 100,
+      },
+    });
+  }
+});
